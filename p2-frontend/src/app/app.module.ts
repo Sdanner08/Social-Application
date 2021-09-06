4c7b9ed3edf6409f7b6f0d927893e34a6172a338
trevdrury <trevdrury@gmail.com>
2021-09-06 10:06:28 -0700

    added folders for pages, services, models, and components

--- p2-frontend/src/app/app.module.ts
+++ p2-frontend/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './pages/index/index.component';
+import { ProfileCreateComponent } from './pages/profile-create/profile-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    ProfileCreateComponent
   ],
   imports: [
     BrowserModule
