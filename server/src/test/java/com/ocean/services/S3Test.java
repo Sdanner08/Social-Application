7a2ba18458bc91cd3927fc647ed4b50223c5d912
David Burton <david.burton@revature.net>
2021-09-17 14:37:31 -0500

    Tried to do S3Tests

--- server/src/test/java/com/ocean/services/S3Test.java
+++ server/src/test/java/com/ocean/services/S3Test.java
@@ -29,3 +29,40 @@ public class S3Test {
     }
 
 }*/
+
+
+
+/*
+David: The above tests functioned when we were still passing files to S3Services, but I couldn't figure out
+how to properly mock MultiPartFile objects.
+
+
+@Test
+    public void uploadImage(){
+        File imageTest = new File("ImageTest.jpg");
+
+        MultipartFile image = new MockMultipartFile(imageTest.getName(), imageTest.getName(), "image", null);
+
+        assertTrue(s3.uploadImage(image).getSuccess());
+    }
+
+    @Test
+    public void uploadProfileImage(){
+        File profileTest = new File("ProfileTest.png");
+
+        MultipartFile profileImage;
+
+        s3.uploadProfileImage(profileImage);
+    }
+
+    //This is the same method in S3Service
+    private File convertMultiPartFileToFile(MultipartFile file) {
+        File convertedFile = new File(file.getOriginalFilename());
+        try (FileOutputStream fos = new FileOutputStream(convertedFile)) {
+            fos.write(file.getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.out.println("Error in S3Service.convertMultiPartFileToFile");
+        }
+        return convertedFile;
+    }*/
